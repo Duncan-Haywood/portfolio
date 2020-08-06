59583b7db86365f1e02976161dfe1efb10dccb38
Duncan Haywood <duncanhaywoodacademic@gmail.com>
2020-08-05 18:18:27 -0700

    shortens lines for readability

--- src/App.js
+++ src/App.js
@@ -45,16 +45,16 @@ const colorSchemes = [
 const intro = (
 	<>
 		<p><strong>Welcome!</strong> 
-			Thank you so much for visiting; 
-			It means a lot to me that you're engaging in my passions.</p>
+			 Thank you so much for visiting; 
+			 It means a lot to me that you're engaging in my passions.</p>
 		<p>This is one of my first large scale projects that I have hosted. 
-			And, if you'd like to check,
-			I'll be sharing more art projects here over time.</p>
+			 And, if you'd like to check,
+			 I'll be sharing more art projects here over time.</p>
 		<p>This project was built by the amazing 
-			<a href="http://moc.co">Joen</a> 
-			who shared this 
-			<a href="https://github.com/jasmussen/turtleshell">project</a> 
-			as open source.</p>
+			 <a href="http://moc.co">Joen</a> 
+			 who shared this 
+			 <a href="https://github.com/jasmussen/turtleshell">project</a> 
+			 as open source.</p>
 		<p>The rest of these pages have some cool animations and quotes 
 			created by Joen.
 			</p>
