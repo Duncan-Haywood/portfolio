f4ba2f081f7505f613255ebd588ce32f2c8be5b7
Duncan Haywood <duncanhaywoodacademic@gmail.com>
2020-08-24 13:20:25 -0700

    adds test.todo and describe for color helper functions

--- src/tests/HeuristicScene.test.js
+++ src/tests/HeuristicScene.test.js
@@ -52,7 +52,50 @@ describe("HelperFunctions Navigation", () => {
 		[prev, next] = handlesEdgeCaseNavigation(current, prev, next, heuristics);
 		return [prev, next]*/
 })
+describe("HelperFunctions colors", () => {
+	test.todo("seed")
+	/*	( number ) {
+	number += 1138;
+	let seed = Math.sin( number ++ ) * 10000;
+	seed = seed - Math.floor( seed );
+	return seed;
+	}*/
 
+	test.todo("getRandomColor")
+	/*( seedNumber, scheme, colorSchemes=colorSchemesConst ) {
+	// seedNumber is the number to be randomized.
+	// scheme is the scheme from the array of color arrays to use.
+
+	// Note to self: by picking a number between 0 and 1 as the seed, 
+	// multiplying by the array length,
+	// it means you'll always pick a number in the array.
+
+	// Non-random color scheme.
+	// The % modulo operator ensures that the array starts again 
+	// from zero when a larger than array.length number is fed.
+	// 
+	// This makes it cycle through the available color arrays, 
+	// one after the other, and cycle when the array ends.
+	let colorScheme = scheme % colorSchemes.length;
+
+	// Store random color from that scheme.
+	let colorFromScheme = Math.floor( seed( seedNumber ) * 
+		colorSchemes[ colorScheme ][2].length );
+
+	return colorSchemes[ colorScheme ][2][ colorFromScheme ];
+	}*/
+
+	test.todo("getDarkColor")/*( scheme, colorSchemes=colorSchemesConst ) {
+	let colorScheme = scheme % colorSchemes.length;
+	return colorSchemes[ colorScheme ][0][0];
+	}*/
+
+	test.todo("getLightColor")/*( scheme, colorSchemes=colorSchemesConst ) {
+	let colorScheme = scheme % colorSchemes.length;
+	return colorSchemes[ colorScheme ][1][0];
+	}*/
+
+})
 describe("Navigation Component", () => {
 
 	test.todo("renders shallowly")
