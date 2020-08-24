fc0c593f0f21a81524fb6b73f4b42207f581cacd
Duncan Haywood <duncanhaywoodacademic@gmail.com>
2020-08-23 21:21:37 -0700

    adds initial failing test to heuristics and adds imports; to be fixed

---
+++ src/tests/HeuristicScene.test.js
@@ -0,0 +1,8 @@
+import { configure } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+configure({ adapter: new Adapter() });
+
+it("has no instantiated tests yet, and we are passing it anyways"), () => {
+	test.todo()
+	//unistantiated
+}
