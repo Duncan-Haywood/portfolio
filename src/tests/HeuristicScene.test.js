c809a020fe72956bdcd499ec9ba6e71367f1d83b
Duncan Haywood <duncanhaywoodacademic@gmail.com>
2020-08-23 21:40:04 -0700

    correctly implements "test.todo()" from jest for Heuristics

--- src/tests/HeuristicScene.test.js
+++ src/tests/HeuristicScene.test.js
@@ -2,7 +2,4 @@ import { configure } from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
 configure({ adapter: new Adapter() });
 
-it("has no instantiated tests yet, and we are passing it anyways"), () => {
-	test.todo()
-	//unistantiated
-}
+test.todo("has no instantiated tests yet, and we are passing it anyways")
