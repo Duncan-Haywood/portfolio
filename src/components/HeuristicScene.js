bbe01401f1a628c0124a942d893383955e0ec365
Duncan Haywood <duncanhaywoodacademic@gmail.com>
2020-08-23 19:06:38 -0700

    removes helper functions to move them to new module

--- src/components/HeuristicScene.js
+++ src/components/HeuristicScene.js
@@ -79,44 +79,6 @@ class Sounds extends React.Component {
 /**
  * Navigation
  */
- const homePageCaseNavigation = (current, next, prev, heuristics)=>{
-	if ( !current ) { 
-	// home page has no id
-		prev = heuristics.length; 
-		// previous page button goes to the last page in the web app
-		next = 1;
-		// next page button goes to the first page in the app 
-		// (the one with the entry)
-	}
-	return [prev, next]
-}
-const firstPageCaseNavigation = (current, prev)=>{
-	if ( current === 1 ) { 
-		//previous page button goes to home page
-		prev = "";
-	}
-	return prev
-}
-const lastPageCaseNavigation = (current, heuristics) =>{
-	if ( current === heuristics.length ) {
-		//next page button goes to home pagge
-		next = "";
-	}
-	return next
-}
-const handlesEdgeCaseNavigation = (current, prev, next, heuristics)=>{
-	[prev, next] = homePageCaseNavigation(current, next, prev, heuristics)
-	prev = firstPageCaseNavigation(current, prev)	
-	next = lastPageCaseNavigation(current, heuristics)
-	return [prev, next]
-}
-const determineNextPrevNavigation = (id, heuristics)=>{
-	let current = parseInt( id );
-	let next = current + 1;
-	let prev = current - 1;
-	[prev, next] = handlesEdgeCaseNavigation(current, prev, next, heuristics);
-	return [prev, next]
-}
 function Navigation( { currentHeuristic, id, playAudio } ) {
 	// id is the page number in the web app
 	// currentHeuristic is ???
