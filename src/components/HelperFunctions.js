387967a7b3c425f3685649eafab15eb23c8be5ec
Duncan Haywood <duncanhaywoodacademic@gmail.com>
2020-08-23 20:12:37 -0700

    comments for explanations on HelperFuntions

--- src/components/HelperFunctions.js
+++ src/components/HelperFunctions.js
@@ -1,6 +1,8 @@
 /*
 * Navigation helper functions
 */
+//Main function at bottom
+//Child of handlesEdgeCaseNavigation -third layer into abstraction
 const homePageCaseNavigation = (current, next, prev, heuristics)=>{
 	if ( !current ) { 
 	// home page has no id
@@ -12,6 +14,7 @@ const homePageCaseNavigation = (current, next, prev, heuristics)=>{
 	}
 	return [prev, next]
 }
+//Child of handlesEdgeCaseNavigation -third layer into abstraction
 const firstPageCaseNavigation = (current, prev)=>{
 	if ( current === 1 ) { 
 		//previous page button goes to home page
@@ -19,6 +22,7 @@ const firstPageCaseNavigation = (current, prev)=>{
 	}
 	return prev
 }
+//Child of handlesEdgeCaseNavigation -third layer into abstraction
 const lastPageCaseNavigation = (current, next, heuristics) =>{
 	if ( current === heuristics.length ) {
 		//next page button goes to home pagge
@@ -26,18 +30,21 @@ const lastPageCaseNavigation = (current, next, heuristics) =>{
 	}
 	return next
 }
+//Second Layer into Abstraction
 const handlesEdgeCaseNavigation = (current, prev, next, heuristics)=>{
 	[prev, next] = homePageCaseNavigation(current, next, prev, heuristics)
 	prev = firstPageCaseNavigation(current, prev)	
 	next = lastPageCaseNavigation(current, next, heuristics)
 	return [prev, next]
 }
+//Second Layer into Abstraction
 const handlesStandardCaseNavigation= (id)=>{
 	let current = parseInt( id );
 	let next = current + 1;
 	let prev = current - 1;
 	return [current, prev, next]
 }
+//Main Function of Use
 const determineNextPrevNavigation = (id, heuristics)=>{
 	let current, prev, next; //page indecies (integers) 
 	[current, prev, next] = handlesStandardCaseNavigation(id);
