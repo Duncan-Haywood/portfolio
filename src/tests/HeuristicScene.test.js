e482ec218454e80824631a7022e2d147be3d2524
Duncan Haywood <duncanhaywoodacademic@gmail.com>
2020-08-24 13:28:10 -0700

    updates imports for Heuristics test

--- src/tests/HeuristicScene.test.js
+++ src/tests/HeuristicScene.test.js
@@ -1,6 +1,6 @@
-import { configure } from "enzyme";
-import Adapter from "enzyme-adapter-react-16";
-configure({ adapter: new Adapter() });
+import React from "react";
+import { shallow, mount } from "enzyme";
+import toJson from "enzyme-to-json";
 
 describe("HelperFunctions Navigation", () => {
 	test.todo("has no instantiated tests yet, and we are passing it anyways")
